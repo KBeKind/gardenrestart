ef3bdb9d6e9e192d5eaf3c0b2a17c97670dc3111
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-06-16 07:35:20 -0500

    initiated new garden project to set it up again

---
+++ src/test/java/org/gardenparty/garden/GardenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gardenparty.garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
